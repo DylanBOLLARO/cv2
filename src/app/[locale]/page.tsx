33debe894c40ed1f23ea8ef7e0c1a5fd290304f3
Bollaro Dylan <bollarodylan@gmail.com>
2025-04-01 17:47:34 +0200

    style: :art: use kebab case name for files

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { AboutMe } from '@/components/AboutMe'
-import { Skills } from '@/components/Skills'
+import { AboutMe } from '@/components/about-me'
+import { Skills } from '@/components/skills'
 
 export default function HomePage() {
     return (
