ee5a1469d8b1ca9a02b1f2e4ec2debe9f238651f
Bollaro Dylan <bollarodylan@gmail.com>
2025-04-02 18:23:24 +0200

    fix: :bug: theme icon is now working with cyber & vintage theme

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -13,14 +13,25 @@ import {
 } from '@/components/ui/dropdown-menu'
 
 export function ModeToggle() {
-    const { setTheme } = useTheme()
+    const { setTheme, theme } = useTheme()
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <Button
+                    variant="outline"
+                    size="icon"
+                    className="transition-all"
+                >
+                    {['light', 'vintage'].includes(theme || '') ? (
+                        <>
+                            <Sun className="h-[1.2rem] w-[1.2rem]" />
+                        </>
+                    ) : (
+                        <>
+                            <Moon className="h-[1.2rem] w-[1.2rem] " />
+                        </>
+                    )}
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
